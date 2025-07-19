b64a1e2574cf92f59e921a37b2b0a1b92853987e
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2025-07-19 18:10:52 -0400

    changes

---
+++ packages/frontend/daka/src/js/partials.js
@@ -0,0 +1,9 @@
+import categoriesPartialUrl from '../pages/categories.html?url'
+
+const partials = {
+  categoriesPartialUrl,
+}
+
+window.partials = partials;
+
+console.log("Partials loaded:", partials);

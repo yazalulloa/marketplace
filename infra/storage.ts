20fa6c5723036057c54ec04805804df8fad56ce6
Yazal Ulloa <yazalulloa@gmail.com>
2025-07-20 13:25:32 -0400

    bcv

--- infra/storage.ts
+++ infra/storage.ts
@@ -4,3 +4,7 @@ export const bucket = new sst.aws.Bucket("MyBucket");
 export const webAssetsBucket = new sst.aws.Bucket("WebAssetsBucket", {
   access: "cloudfront",
 });
+
+export const bcvBucket = new sst.aws.Bucket("BcvBucket", {
+  versioning: false,
+});

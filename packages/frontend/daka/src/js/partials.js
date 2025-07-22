266e17c96689059d108f022386d134468245400c
Betsabe Cohen <betsabe.cohen@geopagos.com>
2025-07-22 10:19:56 -0400

    added carousel

--- packages/frontend/daka/src/js/partials.js
+++ packages/frontend/daka/src/js/partials.js
@@ -1,7 +1,11 @@
 import categoriesPartialUrl from '../pages/categories.html?url'
+import productCarouselPartialUrl from '../pages/products-carousel.html?url'
+import bannerCarouselPartialUrl from '../pages/banner-carousel.html?url'
 
 const partials = {
   categoriesPartialUrl,
+  productCarouselPartialUrl,
+  bannerCarouselPartialUrl,
 }
 
 window.partials = partials;
